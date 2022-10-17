6c209c47c252b261edfc313de7c9004b21f93d4d
t1haju05 <t1haju05@students.oamk.fi>
2022-10-17 16:11:29 +0300

    Removed API key

--- src/weather.js
+++ src/weather.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 const ICON_URL = 'http://openweathermap.org/img/wn/'
-const API_KEY = '103ba146bf9ac0ce210343fa47a38e25'
+const API_KEY = 'Put your api key here'
 
 export default function Weather({ lat, lng }) {
     const [temp, setTemp] = useState(0)
